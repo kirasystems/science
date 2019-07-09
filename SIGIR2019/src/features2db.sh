db4a5aa80dae6b7504a03ecdb95e803aa2f6645b
Edward Lee <edward.lee@kirasystems.com>
2019-07-09 14:36:02 -0400

    Clean up featurization script.

--- SIGIR2019/src/features2db.sh
+++ SIGIR2019/src/features2db.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cat edgar.features | LC_ALL=C sort -T /mnt/data/tmp -S 25G --parallel=4 | LC_ALL=C uniq -c | tee edgar.features.count | ~/features2hashes.py | tee edgar.features.stream | ~/hashes2db.py edgar.hashes.real.db
+# usage: ./features2db.sh <file with all 4grams> <sqlite3 database to write to>
+cat $1 | LC_ALL=C sort -T /mnt/data/tmp -S 25G --parallel=4 | LC_ALL=C uniq -c | tee edgar.features.count | ~/features2hashes.py | tee edgar.features.stream | ~/hashes2db.py $2
